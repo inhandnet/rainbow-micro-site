07ec835624580dbd24311c6999ed690064e0d2dd
yangchao13540012949 <yangchao@inhand.com.cn>
2015-08-27 14:09:52 +0800

    当能上网时一键上网不能点击

--- resource/js/loginpage.js
+++ resource/js/loginpage.js
@@ -1,29 +1,36 @@
 /**
  * Created by yang on 2015/8/26.
  */
+var cloud=new Object();
+var deviceStatus="/api/gateway/client_info"
+var inPortalApiHost="http://api.m.inhand.com.cn:5280"
+checkDeviceStatus();
+//回调
 window.client_info_cb=function(data){
+
     if(data.login=="1"){
-      var a1=  document.getElementById("head-right").href="javascript:void(0)";
+      var a1= document.getElementById("head-right");
         a1.href="javascript:void(0)";
         a1.style.border.top.color="#D3D3D3";
         a1.style.color="#D3D3D3"
     }else if(data.login=="0"){
         //TODO
     }
+
     //TODO
 }
 //每三秒去查询一次设备的登录状态,如果已登录则跳转到登录成功页面
-window.checkDeviceStatus=function(){
-    var uri=Rainbow.cloud.inPortalApiHost+Rainbow.cloud.deviceStatus;
+function checkDeviceStatus(){
+    var uri=inPortalApiHost+deviceStatus;
     var callback="call_back";
     var callbackFuc="client_info_cb";
     var timeout=5000;
     var showTimeout=true;
     var params={};
-    cloud.sendJsonp(uri,callback,callbackFuc,timeout,showTimeout,params);
+  sendJsonp(uri,callback,callbackFuc,timeout,showTimeout,params);
 };
 //发送jsonp请求
-cloud.sendJsonp= function (uri,callback, callbackFuc,timeout,showTimeout, params) {
+function sendJsonp (uri,callback, callbackFuc,timeout,showTimeout, params) {
     $.ajax({
         url:uri,
         dataType:"jsonp",
@@ -46,5 +53,3 @@ cloud.sendJsonp= function (uri,callback, callbackFuc,timeout,showTimeout, params
         }
     });
 };
-Rainbow.cloud.deviceStatus="/api/gateway/client_info"
-Rainbow.cloud.inPortalApiHost="http://api.m.inhand.com.cn:5280"
