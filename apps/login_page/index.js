e67c6c061de5634472c17dc427794d92c1a37f14
j3r0lin <ifjm@qq.com>
2015-10-29 00:53:49 +0800

    add node server for debug in developer's pc.

---
+++ apps/login_page/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+
+app.set('jsonp callback name', 'call_back');
+app.get('/cgi-bin/counting', function (req, res) {
+  res.write("hello world");
+});
+
+app.get('/api/gateway/gateway_info', function (req, res) {
+  res.jsonp({
+    platform: "t.u2wifi.cn",
+    orgId: "123412341234123412341234",
+    loginMethod: "facebook,qq,one-click"
+  });
+});
+
+app.use(express.static("../../", {
+  index: "index.html"
+}));
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
