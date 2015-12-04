16458b9f8ffb8dbd6949d205ebbb686aee112a95
weichunyan <weicy@inhand.com.cn>
2015-12-04 12:07:36 +0800

    登录页面样式修改

--- apps/login_page/index.js
+++ apps/login_page/index.js
@@ -18,7 +18,7 @@ app.use(express.static("../../", {
   index: "index.html"
 }));
 
-var server = app.listen(3000, function () {
+var server = app.listen(2000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
